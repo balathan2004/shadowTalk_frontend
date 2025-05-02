12633b192215bae6cf9880c6e8bc260c91d88c90
light <balathan2004@gmail.com>
2025-05-02 20:28:44 +0530

    cloud test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': '/components',
+      '@utils': '/components/utils'
+    }
+  }
 })
 
 
