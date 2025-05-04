5c33c5fe1944dc21f0ec7e943cbc57a08acb271a
light <balathan2004@gmail.com>
2025-05-04 11:08:17 +0530

    serverUrl added

--- env.ts
+++ env.ts
@@ -1,3 +1,4 @@
-export const serverUrl="http://localhost:4000"
+//export const serverUrl="http://localhost:4000"
+export const serverUrl="https://shadowtalk-server.onrender.com"
 
 //export const serverUrl="http://192.168.133.57:4000"
